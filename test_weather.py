0ac728be4d8cb4a60513374c5a0501b53dc2927e
Josh Friese <Friese@Friese.local>
2017-03-27 18:40:02 -0400

    latest update, working on tests

--- test_weather.py
+++ test_weather.py
@@ -4,4 +4,8 @@ import requests_mock
 
 with requests_mock.Mocker() as m:
     m.get('http://api.wunderground.com/api/844b5d5e3bb67013/condition/forecast10day/astronomy/alerts/currenthurricane/q/27703.json', text='resp')
-    requests.get('http://api.wunderground.com/api/844b5d5e3bb67013/condition/forecast10day/astronomy/alerts/currenthurricane/q/27703.json').text
+    requests.get('http://api.wunderground.com/api/844b5d5e3bb67013/condition/forecast10day/astronomy/alerts/currenthurricane/q/27703.json').json()
+
+
+def test_current_conditions():
+    assert m.text == 'resp'
